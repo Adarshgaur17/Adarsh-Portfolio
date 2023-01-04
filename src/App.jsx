fab80965e3f5a341bdfd4a354a497731a6e86547
Adarsh Gaur <adarshgaur17@gmail.com>
2023-01-04 16:22:37 +0530

    meta tags added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Skills from "./Skills";
+import Projects from "./Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <Skills />
+      <Projects />
     </div>
   );
 }
